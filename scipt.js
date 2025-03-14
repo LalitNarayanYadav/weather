787b1940f447683d14a33366b09f926ad647cdc4
LalitNarayanYadav <narayanlalit710@gmail.com>
2025-03-14 16:17:40 +0530

    Initial commit

---
+++ scipt.js
@@ -0,0 +1,16 @@
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Seattle';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '36ec292418msh923077bdb6bf0e0p1d7dacjsnfc9a4d60ecf4',
+		'x-rapidapi-host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
